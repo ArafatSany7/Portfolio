a315644302dabcdb2b5c2596eec6b74235f3a29f
ArafatSany7 <human.sany7@gmail.com>
2026-01-20 23:56:52 +0600

    Chechk

--- frontend/src/Layout/Main.jsx
+++ frontend/src/Layout/Main.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Shared/Footer/Footer";
+import NavBAr from "../Shared/NavBar/NavBAr";
 
 const Main = () => {
   return (
     <div>
+      <NavBAr></NavBAr>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
---
+++ frontend/src/Shared/NavBar/NavBAr.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NavBAr = () => {
+  return (
+    <div>
+      <h1>This is NavBar</h1>
+    </div>
+  );
+};
+
+export default NavBAr;
