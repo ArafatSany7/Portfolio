f907a65c54adfce17ee9803e6dc88bf1808a41fb
ArafatSany7 <human.sany7@gmail.com>
2026-01-25 23:03:13 +0600

    Nav

--- frontend/src/Components/Nav.jsx
+++ frontend/src/Components/Nav.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import OverlayMenu from "./OverlayMenu";
+import Logo from "../assets/Logo.png";
 
 const Nav = () => {
-  return <div>NavBar.</div>;
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [visible, setVisible] = useState(true);
+  return (
+    <>
+      <nav
+        className={`fixed top-0 left-0 w-full flex items-center justify-center px-6 py-4 z-50 transition-transform duration-300 ${visible ? "translate-y-0" : "-translate-y-full"}`}
+      >
+        <div className="block lg:absolute lg:left-0 lg:transform"></div>
+        <div>
+          <h1 className="text-center">Sany</h1>
+        </div>
+      </nav>
+      <OverlayMenu></OverlayMenu>
+    </>
+  );
 };
 
 export default Nav;
