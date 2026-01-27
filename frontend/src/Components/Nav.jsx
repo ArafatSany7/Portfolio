67fe6c5bd2cc580f38ff6d5eef04cbce6d825286
ArafatSany7 <human.sany7@gmail.com>
2026-01-28 02:06:19 +0600

    nav

--- frontend/src/Components/Nav.jsx
+++ frontend/src/Components/Nav.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import OverlayMenu from "./OverlayMenu";
 import Logo from "../assets/Logo.png";
+import { HiMenu } from "react-icons/hi";
 
 const Nav = () => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -8,14 +9,33 @@ const Nav = () => {
   return (
     <>
       <nav
-        className={`fixed top-0 left-0 w-full flex items-center justify-center px-6 py-4 z-50 transition-transform duration-300 ${visible ? "translate-y-0" : "-translate-y-full"}`}
+        className={`fixed top-0 left-0 w-full flex items-center justify-between px-6 py-4 z-50 transition-transform duration-300 ${visible ? "translate-y-0" : "-translate-y-full"}`}
       >
-        <div className="block lg:absolute lg:left-0 lg:transform"></div>
+        <div>
+          <button
+            onClick={() => setMenuOpen(true)}
+            className="text-white text-3xl focus:outline-none"
+          >
+            <HiMenu />
+          </button>
+        </div>
         <div>
           <h1 className="text-center">Sany</h1>
         </div>
+
+        <div className="hidden lg:block">
+          <a
+            href="#contact"
+            className="bg-linear-to-r from-pink-600 to-blue-600 text-white px-5 py-2 rounded-full font-medium shadow-lg hover:opacity-90 transition-opacity duration-300"
+          >
+            Reachout
+          </a>
+        </div>
       </nav>
-      <OverlayMenu></OverlayMenu>
+      <OverlayMenu
+        isOpen={menuOpen}
+        onClose={() => setMenuOpen(false)}
+      ></OverlayMenu>
     </>
   );
 };
