a990bd8d402a5d7d3e7ef5cebb1070087693eff1
ArafatSany7 <human.sany7@gmail.com>
2026-02-21 04:22:44 +0600

    PROJECT2

--- frontend/src/Section/Project.jsx
+++ frontend/src/Section/Project.jsx
@@ -1,4 +1,9 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
+import photo1 from "../assets/photo1.png";
+import photo2 from "../assets/photo2.png";
+import img1 from "../assets/img1.png";
+import img2 from "../assets/img2.png";
+import { useMotionValueEvent, useScroll } from "framer-motion";
 
 const useIsMobile = (query = "(max-width: 639px)") => {
   const [isMobile, setIsMobile] = useState(
@@ -22,15 +27,47 @@ const Project = () => {
   const isMobile = useIsMobile();
   const sceneRef = useRef(null);
 
-  const project = useMemo(() => [
-    {
-      tittle: "Drop",
-      link: "https://www.drop.com",
-      bgcolor: "#0d4d3d",
-      image: isMobile ? photo1 : img1,
-    },
-  ]);
-  <section id="projects" className="relative text-white"></section>;
+  const project = useMemo(
+    () => [
+      {
+        tittle: "Drop",
+        link: "https://www.drop.com",
+        bgcolor: "#0d4d3d",
+        image: isMobile ? photo1 : img1,
+      },
+      {
+        tittle: "Mesa Elegante",
+        link: "https://www.MesaElegante.com",
+        bgcolor: "#0d4d3d",
+        image: isMobile ? photo2 : img2,
+      },
+    ],
+    [isMobile],
+  );
+
+  const { scrollYProgress } = useScroll({
+    terget: sceneRef,
+    offset: ["start start", "end end"],
+  });
+
+  const thresholds = project.map((_, i) => (i + 1) / project.length);
+
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  useMotionValueEvent(scrollYProgress, "change", (v) => {
+    const idx = thresholds.findIndex((t) => v <= t);
+    setActiveIndex(idx === -1 ? thresholds.length - 1 : idx);
+  });
+
+  const activeProject = project[activeIndex];
+
+  return (
+    <section
+      id="projects"
+      ref={sceneRef}
+      className="relative text-white"
+    ></section>
+  );
 };
 
 export default Project;
