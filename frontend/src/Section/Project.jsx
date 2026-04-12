e6e85c59d6e33022d32967ae376cfb015bf386b7
ArafatSany7 <human.sany7@gmail.com>
2026-04-13 03:40:35 +0600

    PrtojctsSection-portfolio4

--- frontend/src/Section/Project.jsx
+++ frontend/src/Section/Project.jsx
@@ -3,8 +3,7 @@ import img1 from "../assets/img1.png";
 import photo1 from "../assets/photo1.png";
 import img2 from "../assets/img2.png";
 import photo2 from "../assets/photo2.png";
-import { useScroll } from 'framer-motion';
-
+import { useMotionValueEvent, useScroll } from "framer-motion";
 
 const useIsMobileHook = (query = "(max-width : 630px)") => {
   const [isMobile, setMobile] = useState(
@@ -31,22 +30,35 @@ export default function Project() {
         tittle: "Project 1",
         link: "https://www.example.com/",
         bgColor: "#0d4d3d",
-        image: isMobile ?img1  : photo1,
+        image: isMobile ? img1 : photo1,
       },
       {
         tittle: "Project 2",
         link: "https://www.example2.com/",
         bgColor: "#0d4d3d",
         image: isMobile ? img2 : photo2,
-      }
+      },
     ],
     [isMobile],
   );
 
-  const  {scrollYProgress} = useScroll({
-    target:sceneRef,
-    offset:["start start ", "end end"]
-  })
-  const threshold = projects.map((_,i)=>(i+1)/projects.length)
-  return <Section ref={sceneRef} id="projects" className="relative text-white"></Section>;
+  const { scrollYProgress } = useScroll({
+    target: sceneRef,
+    offset: ["start start ", "end end"],
+  });
+  const threshold = projects.map((_, i) => (i + 1) / projects.length);
+  const [activeIndex, setActiveIndex] = useState(0);
+  useMotionValueEvent(scrollYProgress, "change", (v) => {
+    const idx = threshold.findIndex((t) => v <= t);
+    setActiveIndex(idx === -1 ? threshold.length - 1 : idx);
+  });
+  const activeProject = projects[activeIndex];
+  
+  return (
+    <Section
+      ref={sceneRef}
+      id="projects"
+      className="relative text-white"
+    ></Section>
+  );
 }
